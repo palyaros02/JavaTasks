e556b9e0170cc140b5d780c16b87d2f5406fe3b4
palyaros02 <palyaros02@gmail.com>
2020-09-20 22:42:00 +0300

    pr2

---
+++ src/ru/mirea/pr2/Ball.java
@@ -0,0 +1,36 @@
+package ru.mirea.pr2;
+import java.lang.*;
+public class Ball {
+    private double x = 0, y = 0;
+
+    public Ball(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Ball() {
+    }
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setXY(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void move(double xDisp, double yDisp){
+        x += xDisp;
+        y += yDisp;
+    }
+    public String toString(){
+        return "Ball @ ("+this.x+", "+this.y+").";
+    }
+}
+
---
+++ src/ru/mirea/pr2/TestBall.java
@@ -0,0 +1,10 @@
+package ru.mirea.pr2;
+import java.lang.*;
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(100, 100);
+        System.out.println(b1);
+        b1.move(30, 15);
+        System.out.println(b1);
+    }
+}
